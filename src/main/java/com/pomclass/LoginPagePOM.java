e83f100daf1109c20be81d91efbc1d7733049f5d
priyankakulkarni1011 <priyankajanib95@gmail.com>
2024-06-13 13:56:14 +0530

    commit-m "Forgotpassword"

--- src/main/java/com/pomclass/LoginPagePOM.java
+++ src/main/java/com/pomclass/LoginPagePOM.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import com.sample.BaseClass;
+import com.utility.Utility;
 
 public class LoginPagePOM extends BaseClass{
 	
@@ -22,10 +23,10 @@ public class LoginPagePOM extends BaseClass{
 		@FindBy(xpath="//p[text()='Password : admin123']")
 	     private WebElement passwordText;
 		
-		@FindBy(xpath="//input[@name='username']")
+		@FindBy(xpath="//div/input[@name='username']")
 		 private WebElement usernameInput;
 		
-		@FindBy(xpath="//input[@name='password']")
+		@FindBy(xpath="//div/input[@name='password']")
 		 private WebElement passwordInput;
 		
 		@FindBy(xpath="//button[@type='submit']")
@@ -34,21 +35,33 @@ public class LoginPagePOM extends BaseClass{
 		@FindBy(xpath="//div[@class='orangehrm-login-forgot']//child::p")
 		 private WebElement forgotpassword;
 		
+		@FindBy(xpath="//input[@name='username']")
+		private WebElement resetPasswordusernameInput;
+		
 		@FindBy(xpath="//p[text()='Invalid credentials']")
 		 private WebElement errorMessage;
 		
+		@FindBy(xpath="//button[@type='submit']")
+		private WebElement resetPasswordsubmitButton;
+		
+		@FindBy(xpath="//h6[text()='Reset Password link sent successfully']")
+		private WebElement resetPasswordText;
+		
 		public String getUsername() {
-		String username = usernameText.getText();
+		String username = Utility.getWebElementText(usernameText);
 		 return username.split(":")[1].trim();
 		 }
 		public String getPassword() {
-			String password =passwordText.getText();
+			String password = Utility.getWebElementText(passwordText);
 			 return password.split(":")[1].trim();
 		}
+		
 		public void login(String username,String password) {
-			usernameInput.sendKeys(username);
-			passwordInput.sendKeys(password);
-			submitButton.click();
+			
+			Utility.setText(usernameInput, username);
+			Utility.setText(passwordInput, password);
+			
+			Utility.webelementClick(submitButton);
 		}
 		public String getUnvalidUsername() {
 			String username = usernameText.getText();
@@ -59,12 +72,20 @@ public class LoginPagePOM extends BaseClass{
 			 return password.split(":")[0].trim();
 		}
 		public void unvalidLogin(String username,String password) {
-			usernameInput.sendKeys(username);
-			passwordInput.sendKeys(password);
-			submitButton.click();
+			Utility.setText(usernameInput, username);
+			Utility.setText(passwordInput, password);
+			Utility.webelementClick(submitButton);
+			Utility.webelementClick(forgotpassword);
 		
 		}
 		public String getErrorMessage() {
 		  return errorMessage.getText();
 		}
-	}
+		public String forgotPassword(String username,String password) {
+			Utility.webelementClick(forgotpassword);
+			Utility.setText(resetPasswordusernameInput,username );
+			Utility.webelementClick(resetPasswordsubmitButton);
+			String resetText = Utility.getWebElementText(resetPasswordText);
+			return resetText;
+		}
+	}}
--- src/test/java/com/test/LoginPageTest.java
+++ src/test/java/com/test/LoginPageTest.java
@@ -1,8 +1,10 @@
 package com.test;
 
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 import com.pomclass.LoginPagePOM;
 import com.sample.BaseClass;
@@ -22,31 +24,42 @@ public class LoginPageTest extends BaseClass {
 @Test
   public void pageUrl() {
 	 
-	  
-	  String pageurl = getWebDriver().getCurrentUrl();
-	  if(pageurl.equalsIgnoreCase("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")) {
-		  
-		  System.out.println("We are on correct page...Test Pass");
+	   Assert.assertEquals(getWebDriver().getCurrentUrl(),"https://opensource-demo.orangehrmlive.com/web/index.php/auth/login" ); 
+	 
 	  }
-  }
+  
 @Test
 public void loginTest() {
 	LoginPagePOM loginpagepom = new LoginPagePOM();
+	Assert.assertEquals(loginpagepom.getUsername(),"Admin");
+	Assert.assertEquals(loginpagepom.getPassword(), "admin123");
+	
 	loginpagepom.login(loginpagepom.getUsername(),loginpagepom.getPassword());
 }
 @Test
 public void unvalidLoginTest() {
 	LoginPagePOM loginPagePom = new LoginPagePOM();
+	 Assert.assertEquals(loginPagePom.getUnvalidUsername(),"Username");
+	 Assert.assertEquals(loginPagePom.getUnvalidPassword(),"Password");
+	 
 	loginPagePom.unvalidLogin(loginPagePom.getUnvalidUsername(),loginPagePom.getUnvalidPassword());
 	
-   String errorMessage = "Invalid credentials";
-	
-if(loginPagePom.getErrorMessage().equals(errorMessage)) {
-	
-	System.out.println("Test Case Pass");
-	
-}else{
-	System.out.println("Test Case Fail");
+   Assert.assertEquals(loginPagePom.getErrorMessage(), "Invalid credentials"); 
+	}
+
+@Test
+
+  public void forgotPasswordTest() {
+	  LoginPagePOM loginPagePom = new LoginPagePOM();
+	  
+	  SoftAssert assertion = new SoftAssert();
+	  Assert.assertEquals(loginPagePom.getUsername(),"Admin");
+	  
+	  assertion.assertFalse(false,loginPagePom.getPassword());
+	  
+	  loginPagePom.forgotPassword(loginPagePom.getUsername(),loginPagePom.getPassword());
+	  
+	 
+
   }
- }
 }
